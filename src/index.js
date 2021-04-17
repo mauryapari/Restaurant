bbc2ce1779b4c746d6df5314b3704fc30a9a84d2
mauryapari <paritoshmaurya512@gmail.com>
2021-04-17 18:59:37 +0530

    initial Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App/App';
+
+const appElement = document.getElementById('root');
+
+ReactDom.render(<App/>, appElement);
